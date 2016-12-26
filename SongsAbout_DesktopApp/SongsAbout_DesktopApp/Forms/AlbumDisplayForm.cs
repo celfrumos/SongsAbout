7a00023b74cd6003c3b8524c751eb3281e32f7ad
Josh <jdegraw42@gmail.com>
2016-12-26 14:35:58 -0700

    Added reference to actual SpotifyAPI Library files to edit, not tested

--- SongsAbout_DesktopApp/SongsAbout_DesktopApp/Forms/AlbumDisplayForm.cs
+++ SongsAbout_DesktopApp/SongsAbout_DesktopApp/Forms/AlbumDisplayForm.cs
@@ -12,15 +12,21 @@ using SongsAbout.Classes;
 using SongsAbout.Enums;
 using SpotifyAPI.Web.Models;
 
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+
 namespace SongsAbout.Forms
 {
     public partial class AlbumDisplayForm : SongsAbout.Forms.SForm
     {
+        private SpotifyLocalAPI _localApi;
         public AlbumDisplayForm()
         {
             InitializeComponent();
+            _localApi = new SpotifyLocalAPI();
         }
-
+  
         private List<SpotifyTrack> _spotifyTracks;
         private SpotifyAlbum _spotifyAlbum;
         private SpotifyArtist _spotifyArtist;
@@ -72,8 +78,9 @@ namespace SongsAbout.Forms
             {
                 //   this.SpotifyArtist = album.ArtistList[0];
                 this.SpotifyAlbum = album;
-
-
+                SpotifyAPI.Local.Models.Track a = new SpotifyAPI.Local.Models.Track();
+                SpotifyAPI.Local.Models.SpotifyResource r = new SpotifyResource();
+                
                 try
                 {
                     this.sPicturePox1.Image = Importer.ImportSpotifyImage(album.Images[0]);
