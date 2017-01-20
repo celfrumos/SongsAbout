d8b2c4b7311efb37263eb4b36f1ba11be9adf751
Josh <jdegraw42@gmail.com>
2017-01-19 23:53:42 -0700

    Further updated Web, Connected Web to Users table

--- Web/App_Start/Startup.Auth.cs
+++ Web/App_Start/Startup.Auth.cs
@@ -34,7 +34,7 @@ namespace SongsAbout.Web
                         validateInterval: TimeSpan.FromMinutes(30),
                         regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
                 }
-            });            
+            });
             app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
             // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
@@ -55,8 +55,8 @@ namespace SongsAbout.Web
             //   consumerSecret: "");
 
             app.UseFacebookAuthentication(
-               appId: "",
-               appSecret: "");
+               appId: Secrets.Facebook.AppID,
+               appSecret: Secrets.Facebook.SecretID);
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
@@ -64,5 +64,7 @@ namespace SongsAbout.Web
             //    ClientSecret = ""
             //});
         }
+
     }
+
 }
