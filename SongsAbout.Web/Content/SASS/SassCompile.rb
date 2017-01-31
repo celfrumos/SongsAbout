450361ace12aaf33bdf632c0035160253cff881f
Josh <jdegraw42@gmail.com>
2017-01-30 21:41:29 -0700

    Added command line to compile.scss to css b4 build

---
+++ SongsAbout.Web/Content/SASS/SassCompile.rb
@@ -0,0 +1,10 @@
+require "sass"
+options = {
+  cache: true,
+  syntax: :sass,
+  style: :compressed,
+  filename: "site.scss"
+}
+
+render = Sass::Engine.new(File.read("site.scss"), options).render
+File.write("../site2.css", render)
