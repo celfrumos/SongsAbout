7a3d323f743fa73c9b4b91fdaed05235c7300a90
Josh <jdegraw42@gmail.com>
2017-02-02 14:30:55 -0700

    Fixed issue with LocalDB on laptop, created and set up most of schema for .mdf database

--- SongsAbout.Web/Models/IdentityModels.cs
+++ SongsAbout.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SongsAbout.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DatabaseFile", throwIfV1Schema: false)
         {
         }
 
