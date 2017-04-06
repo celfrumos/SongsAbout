62e288e3479cf2366b614ffca1e5c51c426ffeaa
Vm <10642520@INFO3420-10>
2017-04-06 15:48:30 -0600

    More work on searching

--- SongsAbout.Web/Controllers/HomeController.cs
+++ SongsAbout.Web/Controllers/HomeController.cs
@@ -16,29 +16,75 @@ namespace SongsAbout.Web.Controllers
 {
     public partial class HomeController : Controller
     {
+        EntityDbContext db = new EntityDbContext();
         public async Task<ActionResult> Index()
         {
             return View();
         }
 
         [HttpGet]
-        public ActionResult Search(string q, int? type = (int)(SearchMethod.Any))
+        public ActionResult Search(string q, int limit = 5, int? type = (int)(SearchMethod.Any), int? startIndex = 0)
         {
-            var results = _searchRecords(q, (SearchMethod)(type));
+            var results = _searchRecords(q, limit, (SearchMethod)(type));
 
             return View(viewName: "Search", model: results);
         }
 
         [HttpGet]
-        public ActionResult Search(string q, SearchMethod type = SearchMethod.Any)
+        public ActionResult Search(string q, int limit = 5, SearchMethod type = SearchMethod.Any, int? startIndex = 0)
         {
-            var results = _searchRecords(q, type);
+            var results = _searchRecords(q, limit, type);
 
             return View(viewName: "Search", model: results);
         }
-        private SearchResults<SaEntity> _searchRecords(string q, SearchMethod type = SearchMethod.Any)
+
+
+        private SearchResults _searchRecords(string q, int limit = 5, SearchMethod type = SearchMethod.Any, int? startIndex = 0)
         {
-            var results = new SearchResults<SaEntity>();
+            var results = new SearchResults();
+
+
+            if ((type & SearchMethod.Artist) == SearchMethod.Artist || type == SearchMethod.Any)
+            {
+                results.Items.AddRange((from a in db.Artists
+                                        where a.Name.ToLower()
+                                              .Contains(q.ToLower())
+                                        select a).Take(limit));
+
+            }
+            if ((type & SearchMethod.Album) == SearchMethod.Album || type == SearchMethod.Any)
+            {
+                results.Items.AddRange((from a in db.Albums
+                                        where a.Name.ToLower()
+                                              .Contains(q.ToLower())
+                                        select a)
+                                        .Take(limit));
+
+            }
+            if ((type & SearchMethod.Track) == SearchMethod.Track || type == SearchMethod.Any)
+            {
+
+                results.Items.AddRange((from a in db.Tracks
+                                        where a.Name.ToLower()
+                                              .Contains(q.ToLower())
+                                        select a).Take(limit));
+            }
+            if ((type & SearchMethod.Topic) == SearchMethod.Topic || type == SearchMethod.Any)
+            {
+                //results.Items.AddRange((from a in db.Topics
+                //                        where a.TopicText.ToLower()
+                //                              .Contains(q.ToLower())
+                //                        select a).Take(limit));
+
+            }
+            if ((type & SearchMethod.Genre) == SearchMethod.Genre || type == SearchMethod.Any)
+            {
+
+                //results.Items.AddRange((from a in db.Artists
+                //                        where a.Name.ToLower()
+                //                              .Contains(q.ToLower())
+                //                        select a).Take(limit));
+            }
 
             return results;
 
--- SongsAbout.Web/Models/SearchResult.cs
+++ SongsAbout.Web/Models/SearchResult.cs
@@ -11,18 +11,18 @@ namespace SongsAbout.Web.Models
         Artist = 0,
         Album = 1,
         Track = 2,
-        Playlist = 4,
+        Topic = 4,
         Genre = 8,
-        Any = Artist | Album | Track | Playlist | Genre            
+        Any = Artist | Album | Track | Topic | Genre
     }
-    
-    public class SearchResults<T> where T : SaEntity
+
+    public class SearchResults
     {
         public SearchMethod SearchMethod { get; set; } = SearchMethod.Any;
-        public List<T> Results { get; set; }
+        public List<SaEntity> Items { get; set; } = new List<SaEntity>();
         public int StartIndex { get; set; } = -1;
         public int EndIndex { get; set; } = -1;
-        public int Count { get { return this.Results?.Count ?? 0; } }
+        public int Count { get { return this.Items?.Count ?? 0; } }
 
     }
 }
